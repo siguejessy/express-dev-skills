9d6fc306a36ac9eea8e3a30bd59a54595c3084ce
siguejessy <jessicamendez328@gmail.com>
2024-02-17 22:24:36 -0500

    delete a skill functionality & add a skill routed

--- routes/skills.js
+++ routes/skills.js
@@ -5,8 +5,11 @@ const skillsCtrl = require("../controllers/skills");
 router.get("/", skillsCtrl.index);
 router.get("/new", skillsCtrl.new);
 router.get("/:id", skillsCtrl.show);
+router.get("/:id", skillsCtrl.edit);
 
 //All actual paths start with "/skills"
 router.post("/", skillsCtrl.create);
+router.delete("/:id", skillsCtrl.delete);
+router.put("/:id", skillsCtrl.update);
 
 module.exports = router;
