852203458cdf75d1c203c0f892806cca59e57cb0
siguejessy <jessicamendez328@gmail.com>
2024-02-17 20:10:06 -0500

    functional link to new route on index page

--- models/skill.js
+++ models/skill.js
@@ -10,8 +10,27 @@ const skills = [
 
 module.exports = {
   getAll,
+  getOne,
+  create,
 };
 
+// function update(id, updatedSkill) {
+//   id = parseInt(id);
+//   const skill = skills.find((skill) => skill.id === id);
+// Object.assign(skill, updatedSkill);
+// }
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.done = false;
+  skills.push(skill);
+}
+
 function getAll() {
-  return skill;
+  return skills;
+}
+
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find((skill) => skill.id === id);
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var skillsCtrl = require('../controllers/skills');
+const express = require("express");
+const router = express.Router();
+const skillsCtrl = require("../controllers/skills");
 
 router.get("/", skillsCtrl.index);
+router.get("/new", skillsCtrl.new);
 router.get("/:id", skillsCtrl.show);
 
 //All actual paths start with "/skills"
+router.post("/", skillsCtrl.create);
+
 module.exports = router;
