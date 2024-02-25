60675e735b70ca96aa97f59518e857a6e5a7cb0a
Jessica <154856696+siguejessy@users.noreply.github.com>
2024-02-25 03:29:51 -0500

    CRUD functionality for dev skills

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(function (req, res, next) {
   res.locals.time = new Date().toLocaleTimeString();
   next();
